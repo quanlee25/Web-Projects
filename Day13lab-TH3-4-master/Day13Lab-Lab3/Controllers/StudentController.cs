7bd19d02245dea1cc17b1c0bd07b62cb2a772921
quanlee25 <lequan02052005@gmail.com>
2025-11-14 21:39:39 +0700

    bai th

---
+++ Day13lab-TH3-4-master/Day13Lab-Lab3/Controllers/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Day13Lab_Lab3.Models;
+using System.Collections.Generic;
+using System.IO;         
+using System.Linq;
+using System;           
+using System.Threading.Tasks;  
+
+namespace Day13Lab_Lab3.Controllers
+{
+    [Route("Admin/Student")]
+    public class StudentController : Controller
+    {
+        private List<Student> listStudents = new List<Student>();
+        public StudentController()
+        {
+            if (!listStudents.Any()) 
+            {
+                listStudents = new List<Student>()
+                {
+                    new Student() { Id = 101, Name = "Bùi Văn Khoa", Branch = Branch.IT, Gender = Gender.Male, IsRegular = true, Address = "Hà Nội", Email = "k@gmail.com", DateOfBorth = new DateTime(2005, 8, 13), ImagePath="" },
+                    new Student() { Id = 102, Name = "Nguyễn Thị L", Branch = Branch.BE, Gender = Gender.Female, IsRegular = true, Address = "Hà Nội", Email = "lan@gmail.com", DateOfBorth= new DateTime(2004, 5, 21), ImagePath="" },
+                    new Student() { Id = 103, Name = "Trần Văn N", Branch = Branch.CE, Gender = Gender.Male, IsRegular = false, Address = "Hà Nội", Email = "nam@gmail.com", DateOfBorth = new DateTime(2005, 3, 15), ImagePath="" },
+                    new Student() { Id = 104, Name = "Lê Thị H", Branch = Branch.EE, Gender = Gender.Female, IsRegular = true, Address = "Hà Nội", Email = "huong@gmail.com", DateOfBorth = new DateTime(2004, 11, 30), ImagePath="" }
+                };
+            }
+        }
+        [Route("List")]
+        public IActionResult Index()
+        {
+            return View(listStudents);
+        }
+
+        [Route("Add")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem { Value = "IT", Text = "Công nghệ thông tin" },
+                new SelectListItem { Value = "BE", Text = "Kinh tế" },
+                new SelectListItem { Value = "CE", Text = "Công trình" },
+                new SelectListItem { Value = "EE", Text = "Điện, Điện tử" }
+            };
+            return View();
+        }
+        [Route("Add")]
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+                ViewBag.AllBranches = new List<SelectListItem>()
+                {
+                    new SelectListItem { Value = "IT", Text = "Công nghệ thông tin" },
+                    new SelectListItem { Value = "BE", Text = "Kinh tế" },
+                    new SelectListItem { Value = "CE", Text = "Công trình" },
+                    new SelectListItem { Value = "EE", Text = "Điện, Điện tử" }
+                };
+
+                return View(student);
+            }
+
+            // Nếu có upload ảnh
+            if (student.Avatar != null)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+                if (!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+
+                var filePath = Path.Combine(uploadsFolder, student.Avatar.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await student.Avatar.CopyToAsync(stream);
+                }
+
+                student.ImagePath = "/images/" + student.Avatar.FileName;
+            }
+
+            student.Id = listStudents.Max(st => st.Id) + 1;
+            listStudents.Add(student);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
