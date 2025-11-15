bc3f92489342080c1d842ba996213e9968b83dd4
quanlee25 <lequan02052005@gmail.com>
2025-11-16 01:55:07 +0700

    bai th

---
+++ Day14Lab-master/Day13Lab-Lab3/ViewComponents/LeftMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day13Lab_Lab3.Models;
+
+namespace Day13Lab_Lab3.ViewComponents
+{
+    public class LeftMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<MenuItem>
+            {
+                new MenuItem { Id = 1, Name = "Home", Link = Url.Action("Index", "Home") },
+                new MenuItem { Id = 2, Name = "Sinh viên", Link = "/Admin/Student/List" },
+                new MenuItem { Id = 3, Name = "Learner", Link = Url.Action("Index", "Learners") },
+            };
+
+            return View(menuItems); 
+        }
+    }
+}
